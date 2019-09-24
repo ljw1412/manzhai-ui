7b7acb74757faab061ba1ab4368fc5a42c700274
ljw1412 <506644791@qq.com>
2019-09-24 18:58:31 +0800

    基础 List ListItem

--- packages/List/List.vue
+++ packages/List/List.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-@Component
-export default class MzList extends Vue {}
+import { Component, Vue, Prop, Model } from 'vue-property-decorator'
+@Component({
+  provide() {
+    return { mzList: this }
+  }
+})
+export default class MzList extends Vue {
+  @Prop()
+  readonly value!: any
+
+  setValue(value: any, data: any) {
+    this.$emit('input', value)
+    this.$emit('change', value, data)
+  }
+}
 </script>
 
 <style lang="scss">
--- packages/List/ListItem.vue
+++ packages/List/ListItem.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="mz-list-item">
+  <div class="mz-list-item"
+    @click="onItemClick">
+    <div>{{ label || value }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop, Inject } from 'vue-property-decorator'
+
 @Component
-export default class MzListItem extends Vue {}
+export default class MzListItem extends Vue {
+  @Prop()
+  readonly data!: any
+  @Prop(String)
+  readonly label!: string
+  @Prop({ required: true })
+  readonly value!: string
+  @Inject({ from: 'mzList', default: {} })
+  readonly mzList!: any
+
+  onItemClick() {
+    this.$emit('item-click', this.data || this.value)
+    if (this.mzList) {
+      this.mzList.setValue(this.value, this.data)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
