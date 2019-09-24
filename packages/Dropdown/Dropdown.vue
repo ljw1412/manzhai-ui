807f5bafe15ebf153a1909612bee8b49741c9847
ljw1412 <506644791@qq.com>
2019-09-24 16:59:50 +0800

    增加属性值

--- packages/Dropdown/Dropdown.vue
+++ packages/Dropdown/Dropdown.vue
@@ -5,13 +5,18 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop, PropSync } from 'vue-property-decorator'
+
 @Component
 export default class MzDropdown extends Vue {
   @Prop({ default: () => [] })
   readonly list!: Record<string, any>
   @Prop()
   readonly value!: any
+  @Prop({ default: 'value' })
+  readonly valueName!: string
+  @Prop({ default: 'label' })
+  readonly labelName!: string
 }
 </script>
 
