4110e2d1db6e0e750d1e80c53bc96619c32d78d0
ljw1412 <506644791@qq.com>
2019-09-29 17:57:25 +0800

    dropdown v0.1

--- packages/Dropdown/Dropdown.vue
+++ packages/Dropdown/Dropdown.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="mz-dropdown">
-    <slot></slot>
+    <slot :changeVisiable="changeVisiable"></slot>
+    <mz-list :value="value"
+      v-show="mVisiable"
+      @change="onValueChange">
+      <mz-list-item v-for="item of list"
+        ripple
+        :data="item"
+        :key="item[valueName]"
+        :value="item[valueName]"
+        :label="item[labelName] || item[valueName]"></mz-list-item>
+    </mz-list>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop, PropSync } from 'vue-property-decorator'
+import { MzList, MzListItem, MzListGroup } from '../List/index'
 
-@Component
+@Component({
+  components: {
+    MzList,
+    MzListItem,
+    MzListGroup
+  }
+})
 export default class MzDropdown extends Vue {
   @Prop({ default: () => [] })
   readonly list!: Record<string, any>
@@ -17,6 +34,17 @@ export default class MzDropdown extends Vue {
   readonly valueName!: string
   @Prop({ default: 'label' })
   readonly labelName!: string
+
+  mVisiable = false
+
+  onValueChange(value: any, data: any) {
+    this.$emit('input', value)
+    this.$emit('change', value, data)
+  }
+
+  changeVisiable() {
+    this.mVisiable = !this.mVisiable
+  }
 }
 </script>
 
