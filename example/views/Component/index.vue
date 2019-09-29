ff6795b30c9bf44d3aabf535150b06d6186b82f5
ljw1412 <506644791@qq.com>
2019-09-29 19:53:06 +0800

    测试页

--- example/views/Component/index.vue
+++ example/views/Component/index.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="page-component">
-    <mz-dropdown></mz-dropdown>
-    <mz-button type="primary"
+    <mz-dropdown v-model="value"
+      :list="list">
+      <mz-button slot-scope="{changeVisiable}"
+        type="primary"
+        @click="changeVisiable">下拉选择:{{value}}</mz-button>
+    </mz-dropdown>
+    <!-- <mz-button type="primary"
       round
       :ripple="{circle:true,circle:true}"
-      @click="onButtonClick">a</mz-button>
-    <div style="width:200px;border:1px solid #ccc;">
+      @click="onButtonClick">a</mz-button> -->
+
+    <mz-button v-for="type of colorTypeList"
+      :key="type"
+      :type="type"
+      ripple>测试按钮</mz-button>
+    <mz-button disabled
+      ripple>禁用按钮</mz-button>
+
+    <div style="width:200px;border:1px solid #ccc;margin-top:10px">
       <mz-list v-model="value"
         @change="onChange">
         <mz-list-item v-for="item of list"
           ripple
           text="文本"
+          round
           :key="item.value"
           :label="item.label"
           :value="item.value"
@@ -38,6 +52,8 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class PageComponent extends Vue {
+  colorTypeList = ['', 'primary', 'success', 'warning', 'danger', 'info']
+
   list = [
     { label: '测试 1', value: '1' },
     { label: '测试 2', value: '2' },
@@ -61,4 +77,7 @@ export default class PageComponent extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.mz-button + .mz-button {
+  margin-left: 10px;
+}
 </style>
