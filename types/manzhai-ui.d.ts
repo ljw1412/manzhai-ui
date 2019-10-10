ad5e01aad0480ad3c64b4ae14509489cb4a21131
ljw1412 <506644791@qq.com>
2019-10-10 18:50:11 +0800

    修复返回异常

--- types/manzhai-ui.d.ts
+++ types/manzhai-ui.d.ts
@@ -2,8 +2,8 @@ import { VueConstructor } from 'vue'
 
 interface InstallationOptions {}
 
-export function bindDirectives(Vue: VueConstructor)
+export function bindDirectives(Vue: VueConstructor): void
 
-export function bindComponents(Vue: VueConstructor)
+export function bindComponents(Vue: VueConstructor): void
 
-export function install(Vue: VueConstructor, options: InstallationOptions)
+export function install(Vue: VueConstructor, options: InstallationOptions): void
