f7c856294f1e7e4c0835ebdd07cc47cac5670a26
ljw1412 <506644791@qq.com>
2019-10-14 18:47:58 +0800

    完善导航(未完成)

--- packages/List/ListGroup.vue
+++ packages/List/ListGroup.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="mz-list-group">
+    <mz-sub-header>{{label}}</mz-sub-header>
+    <slot></slot>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 @Component
-export default class MzListGroup extends Vue {}
+export default class MzListGroup extends Vue {
+  @Prop()
+  readonly label?: string
+}
 </script>
 
 <style lang="scss">
+.mz-list-group {
+  padding: 0 16px;
+}
 </style>
--- packages/Navigation/Navigation.vue
+++ packages/Navigation/Navigation.vue
@@ -1,15 +1,54 @@
 <script lang="tsx">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 import { MzList, MzListItem, MzListGroup } from '../List/index'
 import { CreateElement } from 'vue'
+import { RawLocation } from 'vue-router'
+import { typeOf } from '../../src/utils/assist'
+
+interface NavigationItem {
+  icon?: string
+  label?: string
+  value: any
+  route?: RawLocation
+  children?: NavigationItem[]
+}
+
+interface NavigationGroup {
+  label?: string
+  list: NavigationItem[]
+}
 
 @Component({
   components: { MzList, MzListItem, MzListGroup }
 })
 export default class MzNavigation extends Vue {
+  @Prop({ default: () => [] })
+  readonly data!: NavigationGroup | NavigationGroup[]
+
   render(h: CreateElement) {
-    const listWrap = <mz-list></mz-list>
-    return <div class="mz-navigation">{listWrap}</div>
+    if (!['object', 'array'].includes(typeOf(this.data)))
+      throw new TypeError('请传入正确的导航栏格式!')
+    let data = this.data
+    if (typeOf(this.data) === 'object') data = [this.data as NavigationGroup]
+
+    const groups = this.renderGroup(data as NavigationGroup[])
+
+    return <div class="mz-navigation">{groups}</div>
+  }
+
+  renderGroup(data: NavigationGroup[]) {
+    return data.map(item => {
+      const listItems = item.list.map(item => {
+        return (
+          <mz-list-item label={item.label} value={item.value}></mz-list-item>
+        )
+      })
+      return (
+        <mz-list-group label={item.label}>
+          <mz-list>{listItems}</mz-list>
+        </mz-list-group>
+      )
+    })
   }
 }
 </script>
