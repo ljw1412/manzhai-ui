ef59684222a3852ad0f889db76d2b4360d105904
ljw1412 <506644791@qq.com>
2019-10-16 18:42:08 +0800

    demo 迁移

--- example/views/Component/List.vue
+++ example/views/Component/List.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="component-list">
+    <mz-list-item-group value="test">
+      <mz-list-item v-for="item of list"
+        link
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        :data="item"></mz-list-item>
+    </mz-list-item-group>
+
+    <div style="width:200px;border:1px solid #ccc;margin-top:10px">
+      <mz-list v-model="value"
+        @change="onChange">
+        <mz-list-item v-for="item of list"
+          text="文本"
+          link
+          round
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+          :data="item"></mz-list-item>
+      </mz-list>
+    </div>
+    <mz-list @change="onChange">
+      <mz-list-item v-for="(item,index) of list"
+        :disabled="index===0"
+        text="文本"
+        :key="item.value"
+        :value="item.value"
+        @click="onItemClick"></mz-list-item>
+    </mz-list>
+    <br />
+    <mz-list-item v-for="item of list"
+      :key="item.value"
+      :value="item.value"
+      @click="onItemClick"></mz-list-item>
   </div>
 </template>
 
@@ -7,7 +42,22 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class ComponentList extends Vue {}
+export default class ComponentList extends Vue {
+  value = '1'
+  list = [
+    { label: '测试 1', value: '1' },
+    { label: '测试 2', value: '2' },
+    { label: '测试 3', value: '3' }
+  ]
+
+  onItemClick(value: any, data: any) {
+    console.log('onItemClick', value, data)
+  }
+
+  onChange(value: any, data: any) {
+    console.log('onChange', value, data)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- example/views/Component/index.vue
+++ example/views/Component/index.vue
@@ -21,7 +21,7 @@
     <!-- <mz-button type="primary"
       round
       :ripple="{circle:true,circle:true}"
-      @click="onButtonClick">a</mz-button> -->
+      >a</mz-button> -->
 
     <!-- <mz-switch v-model="outlined"
       style="margin-right:20px"></mz-switch>
@@ -33,33 +33,7 @@
     <mz-button disabled
       :outlined="outlined"
       ripple>禁用按钮</mz-button> -->
-    <!--
-    <div style="width:200px;border:1px solid #ccc;margin-top:10px">
-      <mz-list v-model="value"
-        @change="onChange">
-        <mz-list-item v-for="item of list"
-          text="文本"
-          link
-          round
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-          :data="item"></mz-list-item>
-      </mz-list>
-    </div>
-    <mz-list @change="onChange">
-      <mz-list-item v-for="(item,index) of list"
-        :disabled="index===0"
-        text="文本"
-        :key="item.value"
-        :value="item.value"
-        @click="onItemClick"></mz-list-item>
-    </mz-list>
-    <br />
-    <mz-list-item v-for="item of list"
-      :key="item.value"
-      :value="item.value"
-      @click="onItemClick"></mz-list-item> -->
+
     <router-view></router-view>
   </div>
 </template>
@@ -70,12 +44,6 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class PageComponent extends Vue {
   colorTypeList = ['', 'primary', 'success', 'warning', 'danger', 'info']
-
-  list = [
-    { label: '测试 1', value: '1' },
-    { label: '测试 2', value: '2' },
-    { label: '测试 3', value: '3' }
-  ]
 
   placementList = [
     { value: 'left' },
@@ -90,18 +58,6 @@ export default class PageComponent extends Vue {
 
   onChangeThemeClick() {
     this.$changeTheme(this.$getCurrentTheme() === 'dark' ? '' : 'dark')
-  }
-
-  onButtonClick() {
-    this.list.pop()
-  }
-
-  onChange(value: any, data: any) {
-    console.log('onChange', value, data)
-  }
-
-  onItemClick(value: any, data: any) {
-    console.log('onItemClick', value, data)
   }
 }
 </script>
