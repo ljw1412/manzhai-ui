000605291b784e4ccc7ab6df8c374c7a256db998
ljw1412 <506644791@qq.com>
2019-10-19 20:52:38 +0800

    优化文档

--- example/views/Component/Button.vue
+++ example/views/Component/Button.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="component-button">
-    <mz-switch v-model="outlined"
-      style="margin-right:20px"></mz-switch>
-    <mz-button v-for="type of colorTypeList"
-      :outlined="outlined"
-      :key="type"
-      :type="type"
-      ripple>测试按钮</mz-button>
-    <mz-button disabled
-      :outlined="outlined"
-      ripple>禁用按钮</mz-button>
+    <div>
+      <span>普通按钮：</span>
+      <mz-button v-for="type of colorTypeList"
+        :outlined="outlined"
+        :key="type"
+        :type="type"
+        ripple>测试按钮</mz-button>
+      <mz-button disabled
+        :outlined="outlined"
+        ripple>禁用按钮</mz-button>
+    </div>
+    <br>
+    <div>
+      <span>线条按钮：</span>
+      <mz-button v-for="type of colorTypeList"
+        outlined
+        :key="type"
+        :type="type"
+        ripple>测试按钮</mz-button>
+      <mz-button disabled
+        outlined>禁用按钮</mz-button>
+    </div>
+    <br>
+    <div>
+      <span>圆角按钮：</span>
+      <mz-button v-for="type of colorTypeList"
+        round
+        :key="type"
+        :type="type"
+        ripple>测试按钮</mz-button>
+      <mz-button round
+        disabled>禁用按钮</mz-button>
+    </div>
+    <br>
+    <div>
+      <span>圆形按钮：</span>
+      <mz-button v-for="type of colorTypeList"
+        circle
+        :key="type"
+        :type="type"
+        ripple>测</mz-button>
+      <mz-button circle
+        disabled>禁</mz-button>
+    </div>
   </div>
 </template>
 
