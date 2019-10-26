c15553354a6f823121386239c2c06ce6c670d905
ljw1412 <506644791@qq.com>
2019-10-26 14:14:33 +0800

    增加 topbar

---
+++ example/components/Topbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="topbar">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Topbar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
