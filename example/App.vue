2fa808ec549ae352fdb3324fe724fd6c23a5fd72
ljw1412 <506644791@qq.com>
2019-10-27 14:13:06 +0800

    提取 topbar

--- example/App.vue
+++ example/App.vue
@@ -1,8 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <topbar id="topbar"></topbar>
+    <div id="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Topbar from './components/Topbar.vue'
+
+@Component({
+  components: {
+    Topbar
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+$topbar-height: 60px;
+#app {
+  height: 100%;
+  overflow: hidden;
+  #topbar {
+    height: $topbar-height;
+    background-color: var(--color-primary);
+    box-shadow: 0 -4px 10px var(--color-primary);
+  }
+  #content {
+    height: calc(100% - #{$topbar-height});
+  }
+}
 </style>
--- example/components/Topbar.vue
+++ example/components/Topbar.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="topbar">
+  <div class="app-topbar flex-center-space-between">
+    <ul class="app-topbar__navigation">
+      <li id="app-logo"> <span class="app-topbar__logo">ManZhai</span></li>
+      <li v-for="(navigation,index) of navigations"
+        :key="index">
+        <router-link :to="navigation.route">{{navigation.label}}</router-link>
+      </li>
+    </ul>
+    <div>
+      <mz-button type="success"
+        size="small"
+        @click="onChangeThemeClick">切换主题</mz-button>
+    </div>
   </div>
 </template>
 
@@ -7,8 +19,52 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Topbar extends Vue {}
+export default class Topbar extends Vue {
+  navigations = [
+    { label: '指南', route: { name: 'pageGuide' } },
+    { label: '组件', route: { name: 'pageComponent' } },
+    { label: '主题', route: { name: 'pageTheme' } }
+  ]
+
+  onChangeThemeClick() {
+    this.$changeTheme(this.$getCurrentTheme() === 'dark' ? '' : 'dark')
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+#app-logo {
+  padding: 0;
+  margin-left: 5px;
+  margin-right: 20px;
+}
+
+.app-topbar {
+  user-select: none;
+  box-sizing: border-box;
+  padding: 10px 15px;
+  &__logo {
+    line-height: 24px;
+    font-size: 24px;
+    color: var(--color-text-primary);
+  }
+
+  &__navigation {
+    display: flex;
+    align-items: center;
+    li {
+      flex: 0 1 auto;
+      font-size: 16px;
+      padding: 0 20px;
+    }
+    li + li {
+      margin-left: 10px;
+    }
+    &::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+  }
+}
 </style>
