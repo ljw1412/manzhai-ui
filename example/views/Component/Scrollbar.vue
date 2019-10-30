fbb0e0ce2522861a984832aeb4d05ee77899ecba
ljw1412 <506644791@qq.com>
2019-10-30 18:01:43 +0800

    demo 文本变色

--- example/views/Component/Scrollbar.vue
+++ example/views/Component/Scrollbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component-scrollbar">
-    <mz-scrollbar style="height:200px;width:300px;">
+    <mz-scrollbar style="height:200px;width:300px;color:var(--color-text-primary)">
       <div style="white-space: nowrap;">测试滚动条测试滚动条测试滚动条测试滚动条</div>
       <div style="width:600px;background-color:green;">1</div>
       <p>测试滚动条</p>
