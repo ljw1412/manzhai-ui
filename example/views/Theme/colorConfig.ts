2816fa96bab9f3202426e6a8fa867d0e2509d7ac
ljw1412 <506644791@qq.com>
2019-11-01 11:54:51 +0800

    主题页

---
+++ example/views/Theme/colorConfig.ts
@@ -0,0 +1,49 @@
+export class ColorItem {
+  name: string
+  hex: string
+  label: string
+  fontColor: string = 'var(--color-text-primary)'
+
+  constructor(name: string, label: string, fontColor?: string) {
+    this.name = name
+    this.label = label
+    this.hex = this.getHex(name)
+    if (fontColor) this.fontColor = fontColor
+  }
+
+  getHex(name: string) {
+    const root = getComputedStyle(document.documentElement)
+    return root.getPropertyValue(name).trim()
+  }
+}
+
+export default [
+  {
+    title: 'Color',
+    colorList: [
+      new ColorItem('--color-primary', '主题色'),
+      new ColorItem('--color-success', '成功色'),
+      new ColorItem('--color-warning', '警告色'),
+      new ColorItem('--color-danger', '危险色'),
+      new ColorItem('--color-info', '信息色')
+    ]
+  },
+  {
+    title: 'FontColor',
+    colorList: [
+      new ColorItem('--color-text-primary', '主色'),
+      new ColorItem('--color-text-regular', '常规色'),
+      new ColorItem('--color-text-secondary', '二级色'),
+      new ColorItem('--color-text-placeholder', '占位色')
+    ]
+  },
+  {
+    title: 'BorderColor',
+    colorList: [
+      new ColorItem('--color-border-base', '基础色'),
+      new ColorItem('--color-border-light', '亮色'),
+      new ColorItem('--color-border-lighter', '较亮色'),
+      new ColorItem('--color-border-extra-light', '更亮色')
+    ]
+  }
+]
--- example/views/Theme/index.vue
+++ example/views/Theme/index.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="page-theme">
+
+    <div v-for="(item,index) of colorConfig"
+      :key="index">
+      <div>{{item.title}}</div>
+      <mz-card v-for="color of item.colorList||[]"
+        outlined
+        :key="color.name"
+        :background="`var(${color.name})`"
+        :style="{color:color.fontColor}">{{color.name}} {{color.label}} {{color.hex}}</mz-card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import colorConfig from './colorConfig'
 
 @Component
-export default class PageTheme extends Vue {}
+export default class PageTheme extends Vue {
+  colorConfig = colorConfig
+}
 </script>
 
 <style lang="scss" scoped>
+.page-theme {
+  padding: 10px 10px 0 10px;
+}
 </style>
