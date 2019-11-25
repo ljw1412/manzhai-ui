a212511420b9c9212b99e1ea89af3743181d0e76
ljw1412 <506644791@qq.com>
2019-11-25 22:34:25 +0800

    [Checkbox] 基础功能，选中状态待修复

--- example/views/Component/Checkbox.vue
+++ example/views/Component/Checkbox.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="component-checkbox">
-    <mz-checkbox></mz-checkbox>
+    <form>
+      <mz-checkbox name="checkbox"
+        :trueValue="1"
+        :falseValue="2">测试</mz-checkbox>
+      <mz-checkbox name="checkbox"
+        label="测试">测试</mz-checkbox>
+      <mz-input name="input"
+        value="123"></mz-input>
+      <button type="submit">123</button>
+    </form>
+
   </div>
 </template>
 
--- packages/Checkbox/Checkbox.vue
+++ packages/Checkbox/Checkbox.vue
@@ -1,15 +1,128 @@
 <template>
-  <label class="mz-checkbox">
+  <label class="mz-checkbox"
+    :class="checkboxClasses"
+    @click="onCheckboxClick">
+    <span class="mz-checkbox__icon"></span>
+    <input v-if="trueValue || falseValue"
+      type="checkbox"
+      class="mz-checkbox__input mz-hidden-input"
+      :value="checked"
+      :name="name"
+      :tabIndex="tabIndex"
+      :true-value="trueValue"
+      :false-value="falseValue" />
+    <input v-else
+      type="checkbox"
+      class="mz-checkbox__input mz-hidden-input"
+      :value="label"
+      :name="name"
+      :tabIndex="tabIndex" />
+    <span class="mz-checkbox__label color-transition"
+      role="checkbox">
+      <slot>{{label}}</slot>
+    </span>
   </label>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Mixins } from 'vue-property-decorator'
+import { Component, Vue, Mixins, Prop } from 'vue-property-decorator'
 import FormElement from '@/mixins/FormElement'
 
 @Component
-export default class MzCheckbox extends Mixins(FormElement) {}
+export default class MzCheckbox extends Mixins(FormElement) {
+  @Prop(Boolean)
+  readonly border!: boolean
+  @Prop(Number)
+  readonly tabIndex!: number
+  @Prop()
+  readonly trueValue!: any
+  @Prop()
+  readonly falseValue!: any
+
+  checked = false
+
+  get checkboxClasses() {
+    return {
+      checked: this.checked,
+      disabled: this.disabled,
+      border: this.border
+    }
+  }
+
+  onCheckboxClick() {
+    console.log(this.checked)
+
+    this.checked = !this.checked
+    this.$emit('input', this.value, this.checked)
+    this.$emit('change', this.value, this.checked)
+  }
+}
 </script>
 
 <style lang="scss">
+.mz-checkbox {
+  --mz-checkbox__padding: 0;
+  --mz-checkbox__icon-size: 16px;
+  --mz-checkbox__label-font-size: 14px;
+  --mz-checkbox__label-font-color: var(--color-text-primary);
+  --mz-checkbox__cursor: pointer;
+
+  position: relative;
+  cursor: var(--mz-checkbox__cursor);
+  user-select: none;
+  display: inline-block;
+  color: var(--mz-checkbox__label-font-color);
+  padding: var(--mz-checkbox__padding);
+  margin: 10px 10px 0 0;
+
+  &__icon {
+    position: relative;
+    display: inline-block;
+    width: var(--mz-checkbox__icon-size);
+    height: var(--mz-checkbox__icon-size);
+    box-sizing: border-box;
+    border: 1px solid var(--mz-checkbox__label-font-color);
+    vertical-align: sub;
+    z-index: 1;
+  }
+
+  &__label {
+    position: relative;
+    vertical-align: top;
+    margin-left: 5px;
+    font-size: var(--mz-checkbox__label-font-size);
+    line-height: 1.5;
+    z-index: 1;
+  }
+
+  &.checked {
+    --mz-checkbox__label-font-color: var(--color-primary);
+    .mz-checkbox__icon::after {
+      visibility: visible;
+    }
+  }
+
+  &.disabled {
+    --mz-checkbox__cursor: not-allowed;
+    --mz-checkbox__label-font-color: var(--color-text-placeholder);
+    &.checked {
+      --mz-checkbox__label-font-color: var(--color-primary-light-5);
+    }
+  }
+
+  &.border {
+    --mz-checkbox__padding: 5px 10px;
+    border: 1px solid var(--mz-checkbox__label-font-color);
+    border-radius: 3px;
+  }
+
+  &:not(.checked):not(.disabled):active {
+    opacity: 0.6;
+    transition: opacity 0.2s;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+}
 </style>
