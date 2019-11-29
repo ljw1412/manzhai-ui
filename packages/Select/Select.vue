f0eb50c502cea746b87d4115b2f61fefcd4bcefc
ljw1412 <506644791@qq.com>
2019-11-29 21:12:22 +0800

    [Select] 选择框

--- packages/Select/Select.vue
+++ packages/Select/Select.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="mz-select">
+  <div v-clickoutside="close"
+    class="mz-select"
+    :class="{'mz-select--active':isActive}">
+    <mz-input outlined
+      :append-icon="arrowIcon"
+      :value="value"
+      :readonly="!search"
+      @focus="onFocus"></mz-input>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import MzInput from '../Input'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
-@Component
-export default class MzSelect extends Vue {}
+@Component({
+  components: {
+    MzInput
+  }
+})
+export default class MzSelect extends Vue {
+  @Prop(Boolean)
+  readonly search!: boolean
+
+  value = ''
+  isActive = false
+
+  get arrowIcon() {
+    return this.isActive ? 'md-arrow-dropup' : 'md-arrow-dropdown'
+  }
+
+  onFocus() {
+    this.isActive = true
+  }
+
+  close() {
+    this.isActive = false
+  }
+}
 </script>
 
 <style lang="scss" >
