5649e31b41c50bdb4880f10658131709d8b874e0
ljw1412 <506644791@qq.com>
2019-12-19 17:53:28 +0800

    临时增加版权信息

--- example/App.vue
+++ example/App.vue
@@ -4,6 +4,11 @@
     <div id="content">
       <router-view></router-view>
     </div>
+    <div id="copyright">
+      <span></span>
+      <a href="http://www.beian.miit.gov.cn"
+        target="_blank"></a>
+    </div>
   </div>
 </template>
 
@@ -21,6 +26,7 @@ export default class APP extends Vue {}
 
 <style lang="scss" scoped>
 $topbar-height: 60px;
+$copyright-height: 20px;
 #app {
   height: 100%;
   overflow: hidden;
@@ -30,7 +36,22 @@ $topbar-height: 60px;
     box-shadow: 0 -4px 10px var(--color-primary);
   }
   #content {
-    height: calc(100% - #{$topbar-height});
+    height: calc(100% - #{$topbar-height + $copyright-height});
+  }
+  #copyright {
+    font-size: 14px;
+    height: $copyright-height;
+    line-height: $copyright-height;
+    text-align: center;
+    background-color: var(--color-primary);
+    color: var(--app-topbar__navigation-font-color);
+    a {
+      margin-left: 5px;
+      color: var(--app-topbar__navigation-font-color);
+      &:hover {
+        color: var(--color-primary-3);
+      }
+    }
   }
 }
 </style>
