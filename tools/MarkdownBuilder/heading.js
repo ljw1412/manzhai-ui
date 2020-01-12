f2dcdbc78c134c7ee28d9ef7c61419321c18f3e0
ljw1412 <506644791@qq.com>
2020-01-12 15:16:38 +0800

    [tools] 文档生成规则更新

--- tools/MarkdownBuilder/heading.js
+++ tools/MarkdownBuilder/heading.js
@@ -1,16 +1,46 @@
 //  heading_open 渲染策略
 module.exports = md => {
-  const defaultRender =
-    md.renderer.rules.heading ||
-    function(tokens, idx, options, env, self) {
-      return self.renderToken(tokens, idx, options)
-    }
-
+  const defaultRender = function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options)
+  }
   md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
     token = tokens[idx]
     token.attrJoin('class', 'mz-header')
     token.attrJoin('class', 'mz-document-header')
     token.attrJoin('data-level', token.tag.replace('h', ''))
+    const nextToken = tokens[idx + 1]
+    if (nextToken && nextToken.type === 'inline' && nextToken.children) {
+      const anchorToken = nextToken.children.find(
+        item =>
+          item.type === 'link_open' &&
+          item.attrGet('class') &&
+          item.attrGet('class').includes('mz-document-anchor')
+      )
+      const textToken = nextToken.children.find(item => item.type === 'text')
+      if (textToken) {
+        textToken.tag = 'div'
+        textToken.attrJoin('class', 'mz-header__title')
+        if (anchorToken) {
+          anchorToken.attrJoin('title', textToken.content)
+        }
+      }
+    }
     return defaultRender(tokens, idx, options, env, self)
+  }
+
+  const defaultTextRender =
+    md.renderer.rules.text ||
+    function(tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options)
+    }
+  md.renderer.rules.text = (tokens, idx, options, env, self) => {
+    const token = tokens[idx]
+    if (
+      token.attrGet('class') &&
+      token.attrGet('class').includes('mz-header__title')
+    ) {
+      return `<div class="mz-header__title">${token.content}</div>`
+    }
+    return defaultTextRender(tokens, idx, options, env, self)
   }
 }
