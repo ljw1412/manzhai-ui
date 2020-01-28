f83d94b636905d5fd688e202915f812a3885df0b
ljw1412 <506644791@qq.com>
2020-01-28 14:20:52 +0800

    [FixedSection] 初步属性定义

--- packages/FixedSection/FixedSection.vue
+++ packages/FixedSection/FixedSection.vue
@@ -1,14 +1,44 @@
-<template>
-  <div class="mz-fixed-section">
-  </div>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+<script lang="tsx">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { CreateElement } from 'vue'
+import BaseAttribute from '@/mixins/BaseAttribute'
 
 @Component
-export default class MzFixedSection extends Vue {}
+export default class MzFixedSection extends BaseAttribute {
+  @Prop(Boolean)
+  readonly placeholder!: boolean
+  @Prop(Boolean)
+  readonly sticky!: boolean
+  @Prop(String)
+  readonly top!: string
+  @Prop(String)
+  readonly bottom!: string
+  @Prop(String)
+  readonly left!: string
+  @Prop(String)
+  readonly right!: string
+  @Prop({ type: Number, default: 900 })
+  readonly zIndex!: number
+
+  render(h: CreateElement) {
+    const section = this.renderSection(h)
+    const placeholderWrapper = <div>{section}</div>
+    return this.placeholder ? placeholderWrapper : section
+  }
+
+  renderSection(h: CreateElement) {
+    const data = {
+      class: ['mz-fixed-section', { 'is-sticky': this.sticky }],
+      style: [this.baseStyles, { zIndex: this.zIndex }],
+      ref: 'section'
+    }
+    const section = <div {...data}></div>
+  }
+}
 </script>
 
-<style lang="scss" >
+<style lang="scss">
+.mz-fixed-section:not(.is-sticky) {
+  position: fixed;
+}
 </style>
