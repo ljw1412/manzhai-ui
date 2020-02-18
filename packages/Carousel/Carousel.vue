6c720bb221ad068ce67ad30fde3691d024ff3d5a
ljw1412 <506644791@qq.com>
2020-02-18 17:05:58 +0800

    [Carousel] 完善指示器

--- packages/Carousel/Carousel.vue
+++ packages/Carousel/Carousel.vue
@@ -1,22 +1,9 @@
-<template>
-  <div class="mz-carousel"
-    :class="{'mz-carousel--vertical': vertical}"
-    :style="[baseStyles]">
-    <slot></slot>
-    <slot name="indicator"
-      :index="index"
-      :item="currentItem"
-      :length="itemCount">
-      <mz-carousel-indicator></mz-carousel-indicator>
-    </slot>
-  </div>
-</template>
-
-<script lang="ts">
+<script lang="tsx">
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 import BaseAttribute from '@/mixins/BaseAttribute'
 import MzCarouselItem from './CarouselItem.vue'
-import MzCarouselIndicator from './CarouselIndicator.vue'
+import MzCarouselIndicator, { PlacementTypes } from './CarouselIndicator.vue'
+import { CreateElement } from 'vue'
 
 @Component({
   provide() {
@@ -37,8 +24,10 @@ export default class MzCarousel extends BaseAttribute {
   readonly reverseTransition!: string
   @Prop(Boolean)
   readonly vertical!: boolean
-  @Prop(Boolean)
-  readonly indicator!: boolean
+  @Prop({ type: String, default: 'none' })
+  readonly indicator!: 'none' | 'line' | 'full-line' | 'circle'
+  @Prop({ type: String, default: 'bottom' })
+  readonly indicatorPlacement!: PlacementTypes
 
   index = -1
   itemList: MzCarouselItem[] = []
@@ -54,16 +43,64 @@ export default class MzCarousel extends BaseAttribute {
     return this.itemList.length
   }
 
+  renderCarousel() {
+    const props = {
+      class: ['mz-carousel', { 'mz-carousel--vertical': this.vertical }],
+      style: [this.baseStyles]
+    }
+    return <div {...props}>{this.$slots.default}</div>
+  }
+
+  renderIndicator() {
+    if (this.$scopedSlots.indicator) {
+      return this.$scopedSlots.indicator({
+        item: this.currentItem,
+        index: this.index,
+        pageIndex: this.index + 1,
+        length: this.itemCount
+      })
+    } else if (this.$slots.indicator) {
+      return this.$slots.indicator
+    } else if (['line', 'full-line', 'circle'].includes(this.indicator)) {
+      return (
+        <mz-carousel-indicator
+          type={this.indicator}
+          placement={this.indicatorPlacement}
+        ></mz-carousel-indicator>
+      )
+    }
+    return null
+  }
+
+  render(h: CreateElement) {
+    return (
+      <div class="mz-carousel-wrapper">
+        {this.renderCarousel()}
+        {this.renderIndicator()}
+      </div>
+    )
+  }
+
   start() {
+    this.stop()
     this.timer = setTimeout(() => {
       this.setActiveIndex(this.index + 1)
       this.start()
     }, this.delay)
   }
 
+  stop() {
+    this.timer && clearTimeout(this.timer)
+  }
+
   setActiveIndex(index: number) {
     const len = this.itemList.length
     this.index = index % len
+  }
+
+  setIndexAndRestartTimer(index: number) {
+    this.setActiveIndex(index)
+    this.start()
   }
 
   @Watch('index')
@@ -83,12 +120,18 @@ export default class MzCarousel extends BaseAttribute {
   }
 
   beforeDestroy() {
-    this.timer && clearTimeout(this.timer)
+    this.stop()
   }
 }
 </script>
 
 <style lang="scss">
+.mz-carousel-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
 .mz-carousel {
   position: relative;
   width: 100%;
--- packages/Carousel/CarouselIndicator.vue
+++ packages/Carousel/CarouselIndicator.vue
@@ -3,12 +3,20 @@ import { Component, Vue, Inject, Prop } from 'vue-property-decorator'
 import { MzCarousel } from '.'
 import { CreateElement } from 'vue'
 
+export type PlacementTypes = 'top' | 'bottom' | 'left' | 'right' | 'outside'
+
 @Component
 export default class MzCarouselIndicator extends Vue {
   @Inject('mzCarousel')
   readonly mzCarousel!: MzCarousel | null
   @Prop({ type: String, default: 'bottom' })
-  readonly placement!: string
+  readonly placement!: PlacementTypes
+  @Prop({ type: String, default: 'line' })
+  readonly type!: 'line' | 'full-line' | 'circle'
+  @Prop(String)
+  readonly activeColor!: String
+  @Prop(String)
+  readonly inactiveColor!: String
 
   get indicatorVNodeList() {
     if (!this.mzCarousel) return null
@@ -17,20 +25,122 @@ export default class MzCarouselIndicator extends Vue {
         class: [
           'mz-carousel-indicator',
           { 'mz-carousel-indicator--active': item.active }
-        ]
+        ],
+        style: {
+          backgroundColor: item.active ? this.activeColor : this.inactiveColor
+        },
+        on: {
+          click: () => {
+            this.mzCarousel!.setIndexAndRestartTimer(index)
+          }
+        }
       }
-      return <div {...data}>{index + 1}</div>
+      return <div {...data}></div>
     })
   }
 
   render(h: CreateElement) {
-    return <div class="mz-carousel-indicators">{this.indicatorVNodeList}</div>
+    const data = {
+      class: [
+        'mz-carousel-indicators',
+        `mz-carousel-indicators--${this.type}`,
+        `mz-carousel-indicators--${this.placement}`
+      ]
+    }
+    return <div {...data}>{this.indicatorVNodeList}</div>
   }
 }
 </script>
 
 <style lang="scss">
 .mz-carousel-indicators {
+  --mz-carousel-indicators__item-bg-color: var(--color-primary-5);
+  --mz-carousel-indicators__item-bg-color--active: var(--color-primary);
+
   position: absolute;
+  display: flex;
+  .mz-carousel-indicator {
+    display: inline-block;
+    cursor: pointer;
+    background-color: var(--mz-carousel-indicators__item-bg-color);
+    transition: 0.3s;
+    &--active {
+      background-color: var(--mz-carousel-indicators__item-bg-color--active);
+    }
+  }
+  &--line {
+    .mz-carousel-indicator {
+      width: 20px;
+      height: 2px;
+      margin: 5px;
+    }
+  }
+  &--full-line {
+    width: 100%;
+    height: 4px;
+    .mz-carousel-indicator {
+      flex: 1 0 auto;
+    }
+  }
+  &--top,
+  &--bottom {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &--left,
+  &--right {
+    top: 50%;
+    transform: translateY(-50%);
+    flex-direction: column;
+    &.mz-carousel-indicators--line {
+      .mz-carousel-indicator {
+        width: 2px;
+        height: 20px;
+        margin: 5px;
+      }
+    }
+    &.mz-carousel-indicators--full-line {
+      width: 4px;
+      height: 100%;
+      .mz-carousel-indicator {
+        flex: 1 0 auto;
+      }
+    }
+  }
+
+  &--outside {
+    position: static;
+    text-align: center;
+    .mz-carousel-indicator {
+      &:first-child {
+        margin-left: auto;
+      }
+      &:last-child {
+        margin-right: auto;
+      }
+    }
+  }
+
+  &--circle {
+    .mz-carousel-indicator {
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      margin: 5px;
+    }
+  }
+
+  &--left {
+    left: 0;
+  }
+  &--right {
+    right: 0;
+  }
+  &--top {
+    top: 0;
+  }
+  &--bottom {
+    bottom: 0;
+  }
 }
 </style>
