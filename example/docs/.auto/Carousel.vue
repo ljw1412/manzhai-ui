1ca7850102d3b9634d773d19fcf3bb7cc37d0035
ljw1412 <506644791@qq.com>
2020-02-19 14:04:12 +0800

    [docs] 更多自定义指示器事例

--- example/docs/.auto/Carousel.vue
+++ example/docs/.auto/Carousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component-carousel">
-    <div class="update-datetime">文档更新时间：2020-02-18 16:31</div>
+    <div class="update-datetime">文档更新时间：2020-02-19 13:54</div>
     <h2 id="carousel-lun-bo" class="mz-header mz-document-header" data-level="2"><a class="mz-document-anchor" href="#carousel-lun-bo" title="Carousel 轮播" data-level="2" data-href="#carousel-lun-bo">¶</a>Carousel 轮播</h2>
 <p>在一个容器内循环播放内容。</p>
 <h3 id="ji-ben-yong-fa" class="mz-header mz-document-header" data-level="3"><a class="mz-document-anchor" href="#ji-ben-yong-fa" title="基本用法" data-level="3" data-href="#ji-ben-yong-fa">¶</a>基本用法</h3>
@@ -152,31 +152,120 @@ export default {
 <p>可以自由扩展的指示器。</p>
 <demo-block>
         <template #example><carousel-demo4 inline-template><div class="carousel-demo4">
-  <mz-carousel vertical height="400px">
-    <mz-carousel-item style="background-color:#6200ea;"></mz-carousel-item>
-    <mz-carousel-item style="background-color:var(--color-success)"></mz-carousel-item>
-    <mz-carousel-item style="background-color:var(--color-warning)"></mz-carousel-item>
-    <mz-carousel-item style="background-color:var(--color-danger)"></mz-carousel-item>
-    <template #indicator="{item, index, pageIndex, length}">
+  <p>Demo1:</p>
+  <mz-carousel ref="carousel" height="200px">
+    <mz-carousel-item v-for="(item, index) of list"
+      :key="index"
+      :data="item"
+      :style="{'background-color':item.color}">
+      <div style="text-align:center; font-size:40px; color:#fff; line-height:200px;">{{index+1}}</div>
+    </mz-carousel-item>
+    <template #indicator="{index}">
+      <div class="custom-indicator">
+        <div v-for="(item, i) of list" 
+          :key="i"
+          class="custom-indicator__item"
+          :class="{'custom-indicator__item--active':index===i}"
+          @click="switchPage(i)">
+          {{item.title}}
+        </div>
+      </div>
+    </template>
+  </mz-carousel>
+  <p>Demo2:</p>
+  <mz-carousel ref="carousel2" height="200px">
+    <mz-carousel-item v-for="(item, index) of list"
+      :key="index"
+      :data="item"
+      :style="{'background-color':item.color}">
+      <div style="text-align:center; font-size:40px; color:#fff; line-height:200px;">{{index+1}}</div>
+    </mz-carousel-item>
+    <template #indicator="{item, pageIndex, length}">
       <div style="position: absolute; right: 10px; bottom: 5px; color: #fff;">
-        {{pageIndex}}/{{length}}
+        <!-- 此处的item.data与mz-carousel-item组件传入的data属性一致 -->
+        ({{pageIndex}}/{{length}}){{item.data.title}}
       </div>
     </template>
   </mz-carousel>
 </div></carousel-demo4></template>
-        <template #description><p><code>indicator</code>作用域插槽用于自定义指示器，它返回一个对象包含:item(当前轮播到的CarouselItem实例),index(下标)，pageIndex(页数，即index+1),length(总共页数)。</p>
+        <template #description><p><code>indicator</code>作用域插槽用于自定义指示器，它返回一个对象包含:item(当前轮播到的CarouselItem实例),index(当前下标)，pageIndex(当前页数，即index+1),length(总共页数)。</p>
 </template>
-        <template #highlight><pre v-pre><code class="html">&lt;mz-carousel vertical height=&quot;400px&quot;&gt;
-  &lt;mz-carousel-item style=&quot;background-color:#6200ea;&quot;&gt;&lt;/mz-carousel-item&gt;
-  &lt;mz-carousel-item style=&quot;background-color:var(--color-success)&quot;&gt;&lt;/mz-carousel-item&gt;
-  &lt;mz-carousel-item style=&quot;background-color:var(--color-warning)&quot;&gt;&lt;/mz-carousel-item&gt;
-  &lt;mz-carousel-item style=&quot;background-color:var(--color-danger)&quot;&gt;&lt;/mz-carousel-item&gt;
-  &lt;template #indicator=&quot;{item, index, pageIndex, length}&quot;&gt;
-    &lt;div style=&quot;position: absolute; right: 10px; bottom: 5px; color: #fff;&quot;&gt;
-      {{pageIndex}}/{{length}}
+        <template #highlight><pre v-pre><code class="html">&lt;p&gt;Demo1:&lt;/p&gt;
+&lt;mz-carousel ref=&quot;carousel&quot; height=&quot;200px&quot;&gt;
+  &lt;mz-carousel-item v-for=&quot;(item, index) of list&quot;
+    :key=&quot;index&quot;
+    :data=&quot;item&quot;
+    :style=&quot;{'background-color':item.color}&quot;&gt;
+    &lt;div style=&quot;text-align:center; font-size:40px; color:#fff; line-height:200px;&quot;&gt;{{index+1}}&lt;/div&gt;
+  &lt;/mz-carousel-item&gt;
+  &lt;template #indicator=&quot;{index}&quot;&gt;
+    &lt;div class=&quot;custom-indicator&quot;&gt;
+      &lt;div v-for=&quot;(item, i) of list&quot; 
+        :key=&quot;i&quot;
+        class=&quot;custom-indicator__item&quot;
+        :class=&quot;{'custom-indicator__item--active':index===i}&quot;
+        @click=&quot;switchPage(i)&quot;&gt;
+        {{item.title}}
+      &lt;/div&gt;
     &lt;/div&gt;
   &lt;/template&gt;
 &lt;/mz-carousel&gt;
+&lt;p&gt;Demo2:&lt;/p&gt;
+&lt;mz-carousel ref=&quot;carousel2&quot; height=&quot;200px&quot;&gt;
+  &lt;mz-carousel-item v-for=&quot;(item, index) of list&quot;
+    :key=&quot;index&quot;
+    :data=&quot;item&quot;
+    :style=&quot;{'background-color':item.color}&quot;&gt;
+    &lt;div style=&quot;text-align:center; font-size:40px; color:#fff; line-height:200px;&quot;&gt;{{index+1}}&lt;/div&gt;
+  &lt;/mz-carousel-item&gt;
+  &lt;template #indicator=&quot;{item, pageIndex, length}&quot;&gt;
+    &lt;div style=&quot;position: absolute; right: 10px; bottom: 5px; color: #fff;&quot;&gt;
+      &lt;!-- 此处的item.data与mz-carousel-item组件传入的data属性一致 --&gt;
+      ({{pageIndex}}/{{length}}){{item.data.title}}
+    &lt;/div&gt;
+  &lt;/template&gt;
+&lt;/mz-carousel&gt;
+
+&lt;script&gt;
+export default {
+  data() {
+    return {
+      list: [
+        {color:'#6200ea',title:'标题1'},
+        {color:'var(--color-success)',title:'标题2'},
+        {color:'var(--color-warning)',title:'标题3'},
+        {color:'var(--color-danger)',title:'标题4'}
+      ]
+    }
+  },
+
+  methods: {
+    switchPage(index){
+      this.$refs.carousel.setIndexAndRestartTimer(index)
+    }
+  }
+}
+&lt;/script&gt;
+
+&lt;style&gt;
+.custom-indicator{
+  display: flex;
+  background-color: #000;
+  color: #fff;
+}
+.custom-indicator__item{
+  cursor: pointer;
+  flex-grow: 1;
+  text-align: center;
+}
+.custom-indicator__item:not(.custom-indicator__item--active):hover{
+   background-color: #666;
+}
+.custom-indicator__item--active{
+  background-color: #ddd;
+  color: #000;
+}
+&lt;/style&gt;
 </code></pre></template></demo-block>
   </div>
 </template>
@@ -201,9 +290,44 @@ export default {
       placementList: ['top', 'bottom', 'left', 'right', 'outside']
     }
   }
-}, CarouselDemo4: {} }
+}, CarouselDemo4: {
+  data() {
+    return {
+      list: [
+        {color:'#6200ea',title:'标题1'},
+        {color:'var(--color-success)',title:'标题2'},
+        {color:'var(--color-warning)',title:'标题3'},
+        {color:'var(--color-danger)',title:'标题4'}
+      ]
+    }
+  },
+
+  methods: {
+    switchPage(index){
+      this.$refs.carousel.setIndexAndRestartTimer(index)
+    }
+  }
+} }
 }
 </script>
 <style lang="scss">
-
+.carousel-demo4{  
+  .custom-indicator{
+    display: flex;
+    background-color: #000;
+    color: #fff;
+  }
+  .custom-indicator__item{
+    cursor: pointer;
+    flex-grow: 1;
+    text-align: center;
+  }
+  .custom-indicator__item:not(.custom-indicator__item--active):hover{
+     background-color: #666;
+  }
+  .custom-indicator__item--active{
+    background-color: #ddd;
+    color: #000;
+  }
+  }
 </style>
--- example/docs/Carousel.md
+++ example/docs/Carousel.md
@@ -109,18 +109,83 @@ export default {
 
 可以自由扩展的指示器。
 
-:::demo `indicator`作用域插槽用于自定义指示器，它返回一个对象包含:item(当前轮播到的CarouselItem实例),index(下标)，pageIndex(页数，即index+1),length(总共页数)。
+:::demo `indicator`作用域插槽用于自定义指示器，它返回一个对象包含:item(当前轮播到的CarouselItem实例),index(当前下标)，pageIndex(当前页数，即index+1),length(总共页数)。
 ```html
-<mz-carousel vertical height="400px">
-  <mz-carousel-item style="background-color:#6200ea;"></mz-carousel-item>
-  <mz-carousel-item style="background-color:var(--color-success)"></mz-carousel-item>
-  <mz-carousel-item style="background-color:var(--color-warning)"></mz-carousel-item>
-  <mz-carousel-item style="background-color:var(--color-danger)"></mz-carousel-item>
-  <template #indicator="{item, index, pageIndex, length}">
-    <div style="position: absolute; right: 10px; bottom: 5px; color: #fff;">
-      {{pageIndex}}/{{length}}
+<p>Demo1:</p>
+<mz-carousel ref="carousel" height="200px">
+  <mz-carousel-item v-for="(item, index) of list"
+    :key="index"
+    :data="item"
+    :style="{'background-color':item.color}">
+    <div style="text-align:center; font-size:40px; color:#fff; line-height:200px;">{{index+1}}</div>
+  </mz-carousel-item>
+  <template #indicator="{index}">
+    <div class="custom-indicator">
+      <div v-for="(item, i) of list" 
+        :key="i"
+        class="custom-indicator__item"
+        :class="{'custom-indicator__item--active':index===i}"
+        @click="switchPage(i)">
+        {{item.title}}
+      </div>
     </div>
   </template>
 </mz-carousel>
+<p>Demo2:</p>
+<mz-carousel ref="carousel2" height="200px">
+  <mz-carousel-item v-for="(item, index) of list"
+    :key="index"
+    :data="item"
+    :style="{'background-color':item.color}">
+    <div style="text-align:center; font-size:40px; color:#fff; line-height:200px;">{{index+1}}</div>
+  </mz-carousel-item>
+  <template #indicator="{item, pageIndex, length}">
+    <div style="position: absolute; right: 10px; bottom: 5px; color: #fff;">
+      <!-- 此处的item.data与mz-carousel-item组件传入的data属性一致 -->
+      ({{pageIndex}}/{{length}}){{item.data.title}}
+    </div>
+  </template>
+</mz-carousel>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {color:'#6200ea',title:'标题1'},
+        {color:'var(--color-success)',title:'标题2'},
+        {color:'var(--color-warning)',title:'标题3'},
+        {color:'var(--color-danger)',title:'标题4'}
+      ]
+    }
+  },
+
+  methods: {
+    switchPage(index){
+      this.$refs.carousel.setIndexAndRestartTimer(index)
+    }
+  }
+}
+</script>
+
+<style>
+.custom-indicator{
+  display: flex;
+  background-color: #000;
+  color: #fff;
+}
+.custom-indicator__item{
+  cursor: pointer;
+  flex-grow: 1;
+  text-align: center;
+}
+.custom-indicator__item:not(.custom-indicator__item--active):hover{
+   background-color: #666;
+}
+.custom-indicator__item--active{
+  background-color: #ddd;
+  color: #000;
+}
+</style>
 ```
 :::
