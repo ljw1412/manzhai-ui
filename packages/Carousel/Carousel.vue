bfd2aecef34dc2c07289a595069dc1f6f786d077
ljw1412 <506644791@qq.com>
2020-02-19 15:32:51 +0800

    [Carousel] 增加切换箭头

--- packages/Carousel/Carousel.vue
+++ packages/Carousel/Carousel.vue
@@ -24,6 +24,8 @@ export default class MzCarousel extends BaseAttribute {
   readonly reverseTransition!: string
   @Prop(Boolean)
   readonly vertical!: boolean
+  @Prop({ type: String, default: 'hover' })
+  readonly arrow!: 'always' | 'hover' | 'never'
   @Prop({ type: String, default: 'none' })
   readonly indicator!: 'none' | 'line' | 'full-line' | 'circle'
   @Prop({ type: String, default: 'bottom' })
@@ -33,6 +35,7 @@ export default class MzCarousel extends BaseAttribute {
   itemList: MzCarouselItem[] = []
   timer: number | null = null
   initing = true
+  isHover = false
 
   get currentItem() {
     if (this.index === -1) return null
@@ -45,10 +48,52 @@ export default class MzCarousel extends BaseAttribute {
 
   renderCarousel() {
     const props = {
-      class: ['mz-carousel', { 'mz-carousel--vertical': this.vertical }],
-      style: [this.baseStyles]
+      class: [
+        'mz-carousel',
+        { 'mz-carousel--vertical': this.vertical, 'is-hover': this.isHover }
+      ],
+      style: [this.baseStyles],
+      on: {
+        mouseover: () => {
+          this.isHover = true
+        },
+        mouseout: () => {
+          this.isHover = false
+        }
+      }
     }
-    return <div {...props}>{this.$slots.default}</div>
+    return (
+      <div {...props}>
+        {this.$slots.default}
+        {this.renderArrow()}
+      </div>
+    )
+  }
+
+  renderArrow() {
+    if (this.arrow === 'never') return null
+    return ['previous', 'next'].map(item => {
+      const data = {
+        class: ['mz-carousel-arrow', `mz-carousel-arrow--${item}`],
+        on: {
+          click: () => {
+            this.setIndexAndRestartTimer(
+              this.index + (item === 'next' ? 1 : -1),
+              false
+            )
+            this.setItemReverse(item === 'previous')
+          }
+        }
+      }
+      if (this.arrow === 'hover') {
+        return (
+          <transition name="mz-fade">
+            <div v-show={this.isHover} {...data}></div>
+          </transition>
+        )
+      }
+      return <div {...data}></div>
+    })
   }
 
   renderIndicator() {
@@ -85,6 +130,7 @@ export default class MzCarousel extends BaseAttribute {
   start() {
     this.stop()
     this.timer = setTimeout(() => {
+      this.setItemReverse(false)
       this.setActiveIndex(this.index + 1)
       this.start()
     }, this.delay)
@@ -94,13 +140,23 @@ export default class MzCarousel extends BaseAttribute {
     this.timer && clearTimeout(this.timer)
   }
 
-  setActiveIndex(index: number) {
-    const len = this.itemList.length
-    this.index = index % len
+  setItemReverse(reverse: boolean) {
+    this.itemList.forEach(item => {
+      item.reverse = reverse
+    })
   }
 
-  setIndexAndRestartTimer(index: number) {
-    this.setActiveIndex(index)
+  setActiveIndex(index: number, checkReverse?: boolean) {
+    const len = this.itemList.length
+    index = (index + len) % len
+    if (checkReverse) {
+      this.setItemReverse(this.index > index)
+    }
+    this.index = index
+  }
+
+  setIndexAndRestartTimer(index: number, checkReverse = true) {
+    this.setActiveIndex(index, checkReverse)
     this.start()
   }
 
@@ -138,5 +194,58 @@ export default class MzCarousel extends BaseAttribute {
   width: 100%;
   height: 100%;
   overflow: hidden;
+
+  &--vertical {
+    .mz-carousel-arrow {
+      left: 50%;
+      margin-left: -20px;
+      &--previous {
+        transform: rotate(90deg);
+        top: 10px;
+      }
+      &--next {
+        transform: rotate(-90deg) scaleX(-1);
+        bottom: 10px;
+      }
+    }
+  }
+  &:not(&--vertical) {
+    .mz-carousel-arrow {
+      top: 50%;
+      transform: translateY(-50%);
+      &--previous {
+        left: 10px;
+      }
+      &--next {
+        right: 10px;
+      }
+    }
+  }
+}
+
+.mz-carousel-arrow {
+  cursor: pointer;
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background-color: rgba($color: #000000, $alpha: 0.2);
+  border-radius: 50%;
+  transition-duration: 0.5s;
+  &::after {
+    position: absolute;
+    content: '';
+    width: 0;
+    height: 0;
+    margin-left: 6px;
+    margin-top: 13px;
+    border-width: 7px 8px;
+    border-style: solid;
+    border-color: transparent #ffffff transparent transparent;
+    opacity: 0.75;
+  }
+  &--next::after {
+    transform: scaleX(-1);
+    transform-origin: 14px 12px;
+  }
 }
 </style>
