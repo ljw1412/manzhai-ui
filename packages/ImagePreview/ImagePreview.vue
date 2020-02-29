2cff34d51350f8ef0db19e8287ca2a9878a4e71d
ljw1412 <506644791@qq.com>
2020-02-29 19:43:28 +0800

    [ImagePreview] 增加临时关闭逻辑

--- packages/ImagePreview/ImagePreview.vue
+++ packages/ImagePreview/ImagePreview.vue
@@ -4,8 +4,11 @@
     :style="{zIndex: zIndex || mZIndex}"
     :class="{'mz-image-preview--drag': mouseDrag.moving}"
     @mousedown="mouseDrag.start($event)">
+    <!-- 关闭按钮 -->
+    <div class="mz-image-preview__close"
+      @click="close"></div>
 
-    <div class="image"
+    <div class="mz-image-preview__image"
       :style="{transform:`translate(${mouseDrag.x}px,${mouseDrag.y}px)`}">
       <img draggable="false"
         :src="currentImage.url" />
@@ -24,7 +27,7 @@ export type ImageItem = { url: string; title?: string; thumbnail?: string }
 @Component
 export default class MzImagePreview extends Vue {
   @PropSync('visible', Boolean)
-  readonly visibleSync!: boolean
+  visibleSync!: boolean
   @Prop(Boolean)
   readonly thumbnail!: boolean
   @Prop({ type: Array, default: () => [] })
@@ -65,9 +68,16 @@ export default class MzImagePreview extends Vue {
     return this.mImages.length === 1
   }
 
+  close() {
+    this.$emit('close')
+    this.visibleSync = false
+  }
+
   @Watch('visibleSync')
   onVisibleChange(visible: boolean) {
-    if (!visible) return
+    if (!visible) {
+      return
+    }
     if (this.index) {
       this.mIndex = Math.max(Math.min(this.index, this.mImages.length - 1), 0)
     } else if (this.current) {
@@ -88,6 +98,25 @@ export default class MzImagePreview extends Vue {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.75);
+  background-color: rgba(0, 0, 0, 0.5);
+  cursor: grab;
+
+  &--drag {
+    cursor: grabbing;
+  }
+
+  &__close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    opacity: 0.75;
+    &:hover {
+      opacity: 1;
+    }
+  }
 }
 </style>
