91fc76d47ad0dcf1489c4e299f6903b6cb71364d
ljw1412 <506644791@qq.com>
2020-03-14 10:37:03 +0800

    样式修复

--- packages/Button/Button.tsx
+++ packages/Button/Button.tsx
@@ -73,7 +73,7 @@ export default class MzButton extends MzSize {
 
   renderContent() {
     const data = {
-      class: ['mz-button__content'],
+      class: ['mz-button__content', 'flex-double-center'],
       style: [this.contentStyles]
     }
     return <span {...data}>{this.$slots.default}</span>
