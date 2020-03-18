2b6b6a1df55dcd5b2c88760d008387359b195797
ljw1412 <506644791@qq.com>
2020-03-18 19:23:32 +0800

    增加注释，改造为双向队列

--- src/classes/Queue.ts
+++ src/classes/Queue.ts
@@ -1,3 +1,7 @@
+/**
+ * 队列模型类
+ * @description 准确来说是双向队列(Deque)
+ */
 export default class Queue {
   q: any[] = []
   maxLength: number = Infinity
@@ -6,26 +10,52 @@ export default class Queue {
     if (maxLength) this.maxLength = maxLength
   }
 
+  /**
+   * 队列是否为空
+   */
   isEmpty() {
     return this.size() === 0
   }
 
+  /**
+   * 队列是否已满，仅在 maxLength 存在时有效。
+   */
   isFull() {
     return this.size() >= this.maxLength
   }
 
+  /**
+   * 获取当前队列长度
+   */
   size() {
     return this.q.length
   }
 
+  /**
+   * 获取队列
+   */
   queue() {
     return this.q
   }
 
+  /**
+   * 弹出头元素
+   */
   pop() {
     return !this.isEmpty() ? this.q.shift() : null
   }
 
+  /**
+   * 弹出尾元素
+   */
+  popBack() {
+    return !this.isEmpty() ? this.q.pop() : null
+  }
+
+  /**
+   * 尾部插入
+   * @param data
+   */
   push(data: any) {
     if (this.isFull()) {
       console.error('插入数据失败，队列已达长度上限！', data)
@@ -35,16 +65,38 @@ export default class Queue {
     return true
   }
 
+  /**
+   * 头部插入
+   * @param data
+   */
+  pushFront(data: any) {
+    if (this.isFull()) {
+      console.error('插入数据失败，队列已达长度上限！', data)
+      return false
+    }
+    this.q.unshift(data)
+    return true
+  }
+
+  /**
+   * 获取头元素
+   */
   front() {
     if (this.isEmpty()) return null
     return this.q[0]
   }
 
+  /**
+   * 获取尾元素
+   */
   back() {
     if (this.isEmpty()) return null
     return this.q[this.size() - 1]
   }
 
+  /**
+   * 清空队列
+   */
   clear() {
     this.q = []
     return this.q
