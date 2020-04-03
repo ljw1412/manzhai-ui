5f174e8f9d558857f607d794a3b880fc92484cde
ljw1412 <506644791@qq.com>
2020-04-03 12:05:07 +0800

    [docs] 基本使用

--- example/docs/.auto/Loading.vue
+++ example/docs/.auto/Loading.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="component-loading">
-    <div class="update-datetime">文档更新时间：2020-04-02 14:01</div>
+    <div class="update-datetime">文档更新时间：2020-04-03 12:03</div>
     <h2 id="loading-jia-zai" class="mz-header mz-document-header" data-level="2"><a class="mz-document-anchor" href="#loading-jia-zai" title="Loading 加载" data-level="2" data-href="#loading-jia-zai">¶</a>Loading 加载</h2>
 <p>用于页面和区块的加载中状态。</p>
 <h3 id="ji-chu-yong-fa" class="mz-header mz-document-header" data-level="3"><a class="mz-document-anchor" href="#ji-chu-yong-fa" title="基础用法" data-level="3" data-href="#ji-chu-yong-fa">¶</a>基础用法</h3>
+<p>以单纯的加载图标模式显示。</p>
 <demo-block>
         <template #example><loading-demo1 inline-template><div class="loading-demo1">
-  <mz-loading></mz-loading>
+  <mz-loading v-for="i of 7" :mode="i"></mz-loading>
 </div></loading-demo1></template>
         <template #description></template>
-        <template #highlight><pre v-pre><code class="html">&lt;mz-loading&gt;&lt;/mz-loading&gt;
+        <template #highlight><pre v-pre><code class="html">&lt;mz-loading v-for=&quot;i of 7&quot; :mode=&quot;i&quot;&gt;&lt;/mz-loading&gt;
 </code></pre></template></demo-block>
   </div>
 </template>
--- example/docs/components/Loading.md
+++ example/docs/components/Loading.md
@@ -3,8 +3,11 @@
 用于页面和区块的加载中状态。
 
 ### 基础用法
+
+以单纯的加载图标模式显示。
+
 :::demo
 ```html
-<mz-loading></mz-loading>
+<mz-loading v-for="i of 7" :mode="i"></mz-loading>
 ```
 :::
