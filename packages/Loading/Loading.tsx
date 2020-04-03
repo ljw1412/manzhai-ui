564bacfb751043111b02fc0cd13067a9168bb958
ljw1412 <506644791@qq.com>
2020-04-03 12:04:30 +0800

    [Loading] 纯图标模式

--- packages/Loading/Loading.tsx
+++ packages/Loading/Loading.tsx
@@ -1,25 +1,33 @@
-import Vue, { RenderContext } from 'vue'
+import Vue, { RenderContext, CreateElement } from 'vue'
 
-function renderFourColor() {
-  const list = [1, 2, 3, 4]
+// 四色方块
+function renderFourColor(h: CreateElement) {
+  return [1, 2, 3, 4].map(i => {
+    return <i class={['shape', `shape${i}`]}></i>
+  })
+}
+
+function renderCore(h: CreateElement, type: string, mode: number) {
+  const animation = type + '-' + mode
   return (
-    <div class="mz-loading__content">
-      {list.map(item => {
-        return <div></div>
-      })}
-    </div>
+    <span class="mz-loading__core">
+      <span class={['mz-loading__icon', type, animation]}>
+        {renderFourColor(h)}
+      </span>
+    </span>
   )
 }
 
 export default Vue.extend({
   functional: true,
-  render(h, ctx) {
-    console.log(ctx)
-    const slots = ctx.slots().default
+  render(h, ctx: RenderContext) {
+    const { mode = 1, type = 'four-color' } = ctx.props
+    const iconVNode = renderCore(h, type, mode)
 
+    const slot = ctx.slots().default
     const data = {
-      class: ['mz-loading', { 'mz-loading--icon': !slots }]
+      class: ['mz-loading', { 'mz-loading--icon': !slot }]
     }
-    return <div {...data}></div>
+    return <div {...data}>{iconVNode}</div>
   }
 })
