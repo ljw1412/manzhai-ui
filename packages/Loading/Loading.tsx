730196dd7e45dfb74f3d3496b26956e3f4a7c8c8
ljw1412 <506644791@qq.com>
2020-04-04 14:25:09 +0800

    [Loading] 完善

--- packages/Loading/Loading.tsx
+++ packages/Loading/Loading.tsx
@@ -1,4 +1,4 @@
-import Vue, { RenderContext, CreateElement } from 'vue'
+import Vue, { RenderContext, CreateElement, VNodeChildren } from 'vue'
 import MzMask from '../Mask/Mask'
 
 // 四色方块
@@ -9,15 +9,10 @@ function renderFourColor(h: CreateElement) {
 }
 
 // 加载的核心
-function renderCore(
-  h: CreateElement,
-  type: string,
-  mode: number,
-  hasSlot: boolean
-) {
+function renderCore(h: CreateElement, type: string, mode: number) {
   const animation = type + '-' + mode
   return (
-    <span class={['mz-loading__core', { 'absolute-center': hasSlot }]}>
+    <span class="mz-loading__core">
       <span class={['mz-loading__icon', type, animation]}>
         {renderFourColor(h)}
       </span>
@@ -25,28 +20,60 @@ function renderCore(
   )
 }
 
+function getLoadingMask(
+  h: CreateElement,
+  props: Record<string, any>,
+  children: VNodeChildren
+) {
+  return h(MzMask, { props }, children)
+}
+
 export default Vue.extend({
   functional: true,
   render(h, ctx: RenderContext) {
-    const { mode = 1, type = 'four-color', loading = false } = ctx.props
+    const {
+      mode = 1,
+      type = 'four-color',
+      loading = false,
+      inline,
+      tip,
+      tipColor,
+      maskColor
+    } = ctx.props
     const slot = ctx.slots().default
-    let iconVNode = renderCore(h, type, mode, !!slot)
+    let iconVNode = renderCore(h, type, mode)
 
     if (slot) {
-      iconVNode = (
-        <mz-mask absolute visible={loading} z-index={500}>
-          {iconVNode}
-        </mz-mask>
+      iconVNode = getLoadingMask(
+        h,
+        { visible: loading, color: maskColor, absolute: true, zIndex: 500 },
+        [
+          <div class="absolute-center">
+            {iconVNode}
+            {tip && (
+              <div class="mz-loading__text" style={{ color: tipColor }}>
+                {tip}
+              </div>
+            )}
+          </div>
+        ] as VNodeChildren
       )
     }
 
     const data = {
-      class: ['mz-loading', { 'mz-loading--icon': !slot }]
+      class: [
+        'mz-loading',
+        {
+          'mz-loading--inline': inline,
+          'mz-loading--icon': !slot,
+          'mz-loading--waiting': loading
+        }
+      ]
     }
     return (
       <div {...data}>
         {iconVNode}
-        {slot}
+        {slot && <div class="mz-loading__container">{slot}</div>}
       </div>
     )
   }
