e438a322894ecd34e0e33ffa8dfe7f71a9076a52
ljw1412 <506644791@qq.com>
2020-04-16 11:37:25 +0800

    [Input] 改为默认带外边框

--- packages/Input/Input.tsx
+++ packages/Input/Input.tsx
@@ -27,7 +27,7 @@ export default class MzInput extends Mixins(MzSize, FormElement) {
   @Prop(String)
   readonly suffix!: string
   @Prop(Boolean)
-  readonly outlined!: boolean
+  readonly flat!: boolean
   @Prop(Boolean)
   readonly shadow!: boolean
   @Ref('input')
@@ -79,13 +79,13 @@ export default class MzInput extends Mixins(MzSize, FormElement) {
         'mz-input flex align-items-center',
         this.mzSize,
         {
-          prefix,
-          suffix,
-          focused: this.focused,
-          rounded: this.rounded,
-          outlined: this.outlined,
-          disabled: this.disabled,
-          shadow: this.shadow
+          'has-prefix': prefix,
+          'has-suffix': suffix,
+          'is-focused': this.focused,
+          'is-rounded': this.rounded,
+          'is-disabled': this.disabled,
+          'is-shadow': this.shadow,
+          'is-flat': this.flat || this.shadow
         }
       ],
       directives: [
