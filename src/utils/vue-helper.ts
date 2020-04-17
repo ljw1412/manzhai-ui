82408bee3d7c12e7d870124721d4f6a37f68d7f1
ljw1412 <506644791@qq.com>
2020-04-17 11:32:36 +0800

    [utils] 补充注释

--- src/utils/vue-helper.ts
+++ src/utils/vue-helper.ts
@@ -1,4 +1,8 @@
-// 常用用于 函数式组件，设置默认值和类型修复
+/**
+ * 设置默认值和类型修复(常用用于 函数式组件)
+ * @param {Object} defaultProps 默认属性，定义对象内属性类型
+ * @param {Object} props 父组件传入的属性
+ */
 export function extendProps(
   defaultProps: Record<string, any>,
   props: Record<string, any>
@@ -6,6 +10,8 @@ export function extendProps(
   const booleanKeys = Object.keys(defaultProps).filter(
     key => typeof defaultProps[key] === 'boolean'
   )
+  // 修复 Boolean 属性的快捷 true 的写法
+  // 例如: <component isFixed></component>
   for (const key of booleanKeys) {
     if (props[key] || props[key] === '') {
       props[key] = true
