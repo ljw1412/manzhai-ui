79d3bd54bcbe207919fa451bce2ac9c25170f277
ljw1412 <506644791@qq.com>
2020-04-19 17:08:06 +0800

    [Base] 修复入口错误

--- example/main.ts
+++ example/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import manzhaiUI from '../src/index'
+import manzhaiUI from '../src/entry'
 import DemoBlock from './components/DemoBlock.vue'
 import hljs from 'highlight.js'
 
---
+++ src/entry.ts
@@ -0,0 +1,3 @@
+// UI库打包用入口，仅为打包样式用。如果无需样式请直接导入index
+import '@/styles/index.scss'
+export { default } from './index'
