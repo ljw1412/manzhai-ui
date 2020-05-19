22cb87d6efa04776754f9d9aae5a32056ac8d946
ljw1412 <506644791@qq.com>
2020-05-19 09:29:20 +0800

    chore: 优化文件创造者的类型处理方式。

--- tools/ComponentCreater/index.js
+++ tools/ComponentCreater/index.js
@@ -3,18 +3,19 @@ const inquirer = require('inquirer')
 ;(async () => {
   const { name, nameCN, type } = await inquirer.prompt([
     {
+      type: 'list',
       name: 'type',
-      message: '*请输入类型(可选值 component, directive):',
-      validate: str => ['component', 'directive'].includes(str)
+      message: '*请输入类型:',
+      choices: ['component', 'directive']
     },
     {
       name: 'name',
-      message: '*请输入创建的组件名称(大驼峰)',
+      message: '*请输入创建的文件名称(大驼峰)',
       validate: str => /^[A-Z]/.test(str)
     },
     {
       name: 'nameCN',
-      message: '*请输入要组件中文名称',
+      message: '*创建的文件中文名称',
       validate: str => !!str
     }
   ])
