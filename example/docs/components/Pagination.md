c4e1c603f58edcdbea11151bdc0614695bd6d1e0
ljw1412 <506644791@qq.com>
2020-06-11 16:20:35 +0800

    chore: 完善分页组件

--- example/docs/components/Pagination.md
+++ example/docs/components/Pagination.md
@@ -2,7 +2,9 @@
 
 以分页的形式，展示大量的数据。
 
-:::demo
+### 基础用法
+
+:::demo `index/v-model`绑定当前页码。
 ```html
 <mz-pagination v-model="index" 
   :page-size="10" :total="70"></mz-pagination>
@@ -10,17 +12,53 @@
 <mz-pagination v-model="index2" 
   :page-size="10" :total="198"></mz-pagination>
 
+<script>
+export default {
+  data (){
+    return {
+      index: 1,
+      index2: 1
+    }
+  }
+}
+</script>
+```
+:::
+
+### 设置最大显示页码数
+
+:::demo `max`属性设置最大显示页码数，即总页数超过`max`会折叠多余的页码按钮。默认值`7`,可选值`4到20之间的奇数`。
+```html
+<mz-pagination 
+  v-model="index" 
+  :page-size="10"
+  :total="198"
+  :max="13"></mz-pagination>
+
+<script>
+export default {
+  data (){
+    return {
+      index: 1
+    }
+  }
+}
+</script>
+```
+:::
+
+### 圆角和外边框
+
+:::demo `outlined`和`circle`分别控制是否显示外边框和是否为圆角。
+```html
 <mz-pagination v-model="index2" outlined
   :page-size="10" :total="198"></mz-pagination>
 
 <mz-pagination v-model="index2" circle
   :page-size="10" :total="198"></mz-pagination>
 
-<mz-pagination v-model="index2" outlined circle
+<mz-pagination v-model="index3" outlined circle
   :page-size="10" :total="198"></mz-pagination>
-
-<mz-pagination v-model="index3" 
-  :page-size="10" :page-count="100" :max="19"></mz-pagination>
 
 <script>
 export default {
@@ -35,3 +73,105 @@ export default {
 </script>
 ```
 :::
+
+### 自定义布局
+:::demo 通过`layout`可以自由设定分页组件的布局。可选值`prev`,`next`,`sizes`,`pager`,`jumper`,`total`。
+```html
+<span class="demo-font-size-14">带总数</span>
+<mz-pagination 
+  v-model="index" 
+  :layout="['total', 'prev', 'pager', 'next']"
+  :page-size="10" 
+  :total="100"></mz-pagination>
+<span class="demo-font-size-14">自选每页显示条数</span>
+<mz-pagination 
+  v-model="index2" 
+  :layout="['sizes','prev', 'pager', 'next']"
+  :page-size="10" 
+  :total="100"></mz-pagination>
+<span class="demo-font-size-14">跳转到指定页数</span>
+<mz-pagination 
+  v-model="index3" 
+  :layout="['prev', 'pager', 'next', 'jumper']"
+  :page-size="10" 
+  :total="100"></mz-pagination>
+<span class="demo-font-size-14">全部功能</span>
+<mz-pagination 
+  v-model="index4" 
+  :layout="['total', 'sizes', 'prev', 'pager', 'next', 'jumper']"
+  :page-size="10" 
+  :total="100"></mz-pagination>
+
+<script>
+export default {
+  data (){
+    return {
+      index: 1,
+      index2: 1,
+      index3: 1,
+      index4: 1
+    }
+  }
+}
+</script>
+```
+:::
+
+
+### 不同大小
+:::demo
+```html
+<mz-pagination v-model="index" size="small"
+  :page-size="10" :total="100"></mz-pagination>
+
+<mz-pagination v-model="index2" size="medium"
+  :page-size="10" :total="100"></mz-pagination>
+
+<mz-pagination v-model="index3" 
+  :page-size="10" :total="100"></mz-pagination>
+
+<mz-pagination v-model="index4" size="large"
+  :page-size="10" :total="100"></mz-pagination>
+
+<script>
+export default {
+  data (){
+    return {
+      index: 1,
+      index2: 1,
+      index3: 1,
+      index4: 1
+    }
+  }
+}
+</script>
+```
+:::
+
+### API
+
+#### 属性
+| 参数 | 说明 | 类型 | 可选值 |默认值|
+| --- | --- | --- | --- | --- |
+|index/v-model|当前页码|Number||1|
+|pageSize|每页显示条目个数，支持 .sync 修饰符|Number||10|
+|total|总条目数|Number|||
+|pageCount|总页数，与total二选一|Number|||
+|max|最大页码按钮的数量，当总页数超过该值时会折叠|Number|4到20之间的奇数|7|
+|pageSizes|每页显示个数选择器的选项设置，需要配合total属性|number[]||[10, 20, 30, 40, 50]|
+|outlined|是否使用边框模式|Boolean|||
+|circle|是否为圆型|Boolean|||
+|disabled|是否禁用|Boolean|||
+|layout|布局|string[]|`prev`,`next`,`sizes`,`pager`,`jumper`,`total`|['prev', 'pager', 'next']|
+|totalText|总数文案|String||'共 %d 条'|
+|sizeText|每页显示数量的文案|String||'%d 条/页'|
+
+
+#### 事件
+
+| 名称 | 说明 | 参数 |
+| --- | --- | --- |
+|change|当前页码改变事件|(num:Number)|
+|size-change|每页显示个数改变事件|(size:Number)|
+|prev-click|点击上一页改变当前页后触发|(index:Number)|
+|next-click|点击下一页改变当前页后触发|(index:Number)|
--- packages/Pagination/Pagination.tsx
+++ packages/Pagination/Pagination.tsx
@@ -1,5 +1,7 @@
 import { Component, Model, Prop, PropSync } from 'vue-property-decorator'
-import { CreateElement, VNodeData } from 'vue'
+import { CreateElement, VNode } from 'vue'
+import MzSelect from 'manzhai-ui/packages/Select/Select.vue'
+import MzOption from 'manzhai-ui/packages/Select/Option.vue'
 import MzButton from 'manzhai-ui/packages/Button/Button'
 import MzIcon from 'manzhai-ui/packages/Icon/Icon.vue'
 import MzSize from 'manzhai-ui/src/mixins/MzSize'
@@ -7,12 +9,14 @@ import MzPager from './Pager'
 
 const CHEVRON_ICON = { prev: 'chevron-back', next: 'chevron-forward' }
 
-@Component({ components: { MzButton, MzIcon, MzPager } })
+@Component({ components: { MzSelect, MzOption, MzButton, MzIcon, MzPager } })
 export default class MzPagination extends MzSize {
   @Model('index:change', { type: Number, default: 1 })
   readonly index!: number
   @PropSync('pageSize', { type: Number, default: 10 })
-  readonly pageSizeSync!: number
+  pageSizeSync!: number
+  @Prop({ type: Array, default: () => [10, 20, 30, 40, 50] })
+  readonly pageSizes!: number[]
   @Prop(Number)
   readonly pageCount!: number
   @Prop(Number)
@@ -29,8 +33,23 @@ export default class MzPagination extends MzSize {
   readonly circle!: boolean
   @Prop(Boolean)
   readonly disabled!: boolean
-  @Prop({ type: String, default: '共%d条' })
+  @Prop({ type: Array, default: () => ['prev', 'pager', 'next'] })
+  readonly layout!: string[]
+  @Prop({ type: String, default: '共 %d 条' })
   readonly totalText!: string
+  @Prop({ type: String, default: '%d 条/页' })
+  readonly sizeText!: string
+
+  jumpNum: string | number = ''
+
+  renderStore: Record<string, () => VNode | Element | undefined> = {
+    prev: () => this.renderController('prev'),
+    next: () => this.renderController('next'),
+    sizes: () => this.renderPageSizes(),
+    pager: () => this.renderPager(),
+    jumper: () => this.renderJumper(),
+    total: () => this.renderTotal()
+  }
 
   get mPageCount() {
     if (typeof this.total === 'number') {
@@ -56,9 +75,29 @@ export default class MzPagination extends MzSize {
     }
   }
 
+  get renderItems() {
+    return this.layout.map(key => {
+      const render = this.renderStore[key]
+      return render && render()
+    })
+  }
+
   handlePageChange(num: number) {
+    this.jumpNum = num
     this.$emit('change', num)
     this.$emit('index:change', num)
+  }
+
+  renderPager() {
+    return (
+      <mz-pager
+        index={this.index}
+        pageCount={this.mPageCount}
+        max={this.max}
+        size={this.size}
+        buttonProp={this.buttonProp}
+        on-change={this.handlePageChange}></mz-pager>
+    )
   }
 
   renderController(type: 'prev' | 'next') {
@@ -69,7 +108,9 @@ export default class MzPagination extends MzSize {
       props: { ...this.buttonProp, disabled: this.disabled || disabled },
       on: {
         click: () => {
-          this.handlePageChange(this.index + (isPrev ? -1 : 1))
+          const index = this.index + (isPrev ? -1 : 1)
+          this.handlePageChange(index)
+          this.$emit(`${type}-click`, index)
         }
       }
     }
@@ -89,20 +130,67 @@ export default class MzPagination extends MzSize {
     )
   }
 
+  renderPageSizes() {
+    const data = {
+      class: 'mz-pagination-sizes',
+      props: {
+        value: this.pageSizeSync,
+        size: this.size
+      },
+      on: {
+        change: (val: number) => {
+          this.pageSizeSync = val
+          this.$emit('size-change', val)
+        }
+      }
+    }
+
+    return (
+      <mz-select {...data}>
+        {this.pageSizes.map(num => (
+          <mz-option
+            value={num}
+            label={this.sizeText.replace(/%d/g, num + '')}></mz-option>
+        ))}
+      </mz-select>
+    )
+  }
+
+  renderJumper() {
+    const inputData = {
+      props: {
+        value: this.jumpNum,
+        type: 'number',
+        size: this.size
+      },
+      on: { input: (val: number) => (this.jumpNum = val) },
+      nativeOn: {
+        keydown: (event: KeyboardEvent) => {
+          if (event.keyCode === 13) {
+            let num = Number(this.jumpNum)
+            num = Math.max(1, Math.min(num, this.mPageCount))
+            this.handlePageChange(num)
+          }
+        }
+      }
+    }
+
+    return (
+      <span class="mz-pagination-jumper">
+        跳转到<mz-input {...inputData}></mz-input>页
+      </span>
+    )
+  }
+
   render(h: CreateElement) {
     return (
       <div class={['mz-pagination', { 'is-outlined': this.outlined }]}>
-        {this.renderTotal()}
-        {this.renderController('prev')}
-        <mz-pager
-          index={this.index}
-          pageCount={this.mPageCount}
-          max={this.max}
-          size={this.size}
-          buttonProp={this.buttonProp}
-          on-change={this.handlePageChange}></mz-pager>
-        {this.renderController('next')}
+        {this.renderItems}
       </div>
     )
+  }
+
+  mounted() {
+    this.jumpNum = this.index
   }
 }
