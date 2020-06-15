efcd7d2a2e29ba6d2e9b91dfd20b09d56af4f507
ljw1412 <506644791@qq.com>
2020-06-15 14:49:28 +0800

    chore: 重置

--- example/views/Guide/index.vue
+++ example/views/Guide/index.vue
@@ -1,34 +1,5 @@
 <template>
   <div class="page-guide">
-    <mz-checkbox v-model="value2">选择项</mz-checkbox>
-
-    <mz-checkbox-group v-model="value">
-      <mz-checkbox value="A">选择项A</mz-checkbox>
-      <mz-checkbox value="B">选择项B</mz-checkbox>
-    </mz-checkbox-group>
-
-    <mz-radio-group name="radio-group-2"
-      v-model="value2">
-      <mz-radio value="1">选项1</mz-radio>
-      <mz-radio value="2">选项2</mz-radio>
-      <mz-radio value="3">选项3</mz-radio>
-    </mz-radio-group>
-
-    <mz-rate :value="3.7"
-      readonly
-      show-text />
-
-    <mz-select v-model="value"
-      placeholder="目标">
-
-    </mz-select>
-
-    <mz-button color="primary">发多少</mz-button>
-
-    <mz-tag size="small">标签</mz-tag>
-    <mz-tag size="medium">标签</mz-tag>
-    <mz-tag>标签</mz-tag>
-    <mz-tag size="large">标签</mz-tag>
   </div>
 </template>
 
@@ -37,14 +8,6 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class PageGuide extends Vue {
-  countries = [
-    { label: '全部', value: 'All' },
-    { label: '中国', value: 'China' },
-    { label: '日本', value: 'Janpan' },
-    { label: '新加坡', value: 'Singapore' },
-    { label: '美国', value: 'America' },
-    { label: '英国', value: 'England' }
-  ]
 }
 </script>
 
