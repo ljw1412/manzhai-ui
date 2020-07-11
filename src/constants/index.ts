6d903496f5305a433dc19e323c1d5fdd558a3225
ljw1412 <506644791@qq.com>
2020-07-11 16:16:26 +0800

    chore: 增加颜色判断

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,41 @@
 export const COLOR_TYPES = ['primary', 'success', 'warning', 'danger']
+
+const themeColors = [
+  'primary',
+  'secondary',
+  'success',
+  'warning',
+  'danger',
+  'light',
+  'dark'
+]
+
+const colors = [
+  'blue',
+  'indigo',
+  'purple',
+  'pink',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'teal',
+  'cyan',
+  'white',
+  'gray',
+  'gray-dark'
+]
+
+export const PRESET_COLOR_MAP = {
+  bg: [...themeColors, ...colors, 'body', 'transparent'],
+  text: [
+    ...themeColors,
+    ...colors,
+    'body',
+    'muted',
+    'black-50',
+    'white-50',
+    'reset'
+  ],
+  border: [...themeColors, ...colors]
+}
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,4 +1,4 @@
-import { COLOR_TYPES } from '../constants/index'
+import { COLOR_TYPES, PRESET_COLOR_MAP } from '../constants/index'
 /**
  * 切换根节点上的主体名称 (data-theme)
  * @param name 主题名称
@@ -19,6 +19,32 @@ export function getMzColor(color?: string) {
   return color
 }
 
+// @Deprecated
 export function colorInTypes(color?: string) {
   return COLOR_TYPES.includes(color || '')
+}
+
+/**
+ * 是否为预设颜色
+ * @param color 颜色
+ * @param type 类型
+ */
+export function isPresetColor(
+  color?: string,
+  type: 'text' | 'bg' | 'border' = 'text'
+) {
+  if (!color) return false
+  return PRESET_COLOR_MAP[type].includes(color)
+}
+
+/**
+ * 获得预设颜色样式类
+ * @param color 颜色
+ * @param type 类型
+ */
+export function getPresetColorClass(
+  color?: string,
+  type: 'text' | 'bg' | 'border' = 'text'
+) {
+  return isPresetColor(color, type) ? `${type}-${color}` : null
 }
